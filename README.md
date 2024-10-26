c22444ac04a46c79839922c7f741f975ee09a2f5
Ludovico Russo <ludus.russo@gmail.com>
2024-10-26 16:34:06 +0200

    doc: improve Readme

--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 
 [![Open Source Saturday](https://img.shields.io/badge/%E2%9D%A4%EF%B8%8F-open%20source%20saturday-F64060.svg)](https://www.meetup.com/it-IT/Open-Source-Saturday-Milano/)
 
+
+## Usage
+
+```ts
+import { execGraph } from "./alg/traverser.ts";
+import { Graph } from "./alg/types.ts";
+import { z } from "npm:zod";
+
+const graph: Graph = ...;
+const schema = z.record(z.string(), z.any());
+const res = await execGraph(graph, schema, {});
+```
+
+```bash
+$ deno run --allow-run main.ts
+# output { a: 4, b: 'test' }
+```
--- main.ts
+++ main.ts
@@ -1,16 +1,41 @@
+import { execGraph } from "./alg/traverser.ts";
+import type { Graph } from "./alg/types.ts";
 import { VM } from "./vm/vm.ts";
 import { z } from "npm:zod";
 
 const vm = new VM(z.number(), "execute");
 
+const graph: Graph = {
+  nodes: [
+    {
+      id: "start",
+      code: `
+        function execute() { 
+          return { ctx: { a: 2 }, next: "next" }; 
+        }`,
+      targets: ["next"],
+    },
+    {
+      id: "2",
+      code: `
+        function execute(prev: {a: number}) {  
+          return { ctx: { a: 2 * prev.a, b: 'test' } }; 
+        }`,
+      targets: [],
+    },
+  ],
+  edges: [
+    {
+      id: "start-2",
+      source: "next",
+      from: "start",
+      to: "2",
+    },
+  ],
+};
+
 // Example usage
 if (import.meta.main) {
-  const res = await vm.execute(
-    `function execute(x) { 
-      return x+1;
-    }`,
-    10
-  );
-
+  const res = await execGraph(graph, z.record(z.string(), z.any()), {});
   console.log(res);
 }
